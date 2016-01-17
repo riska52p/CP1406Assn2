53943cea86f1c7b68678fb9a004487b921d495c4
TuyetMy <tuyetmy.ho@my.jcu.edu.au>
2016-01-17 17:10:04 +0800

    Orderform updated
    very simple

---
+++ Diazepam/scripts.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+function resetForm() {
+    var reset = confirm('Reset all fields?');
+    if (reset) {
+        console.log("Resetting form modifications.");
+     
+    }
+    return reset;
+}
