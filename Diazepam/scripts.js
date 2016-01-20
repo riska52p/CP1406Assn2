471c088ef497c215697050c2792b905030335bff
Riska Wilian Triany Putri <riska52p@yahoo.com>
2016-01-20 21:51:47 +0800

    The Final Complete Version
    Please do check and tell me if anything was wrong

--- Diazepam/scripts.js
+++ Diazepam/scripts.js
@@ -96,3 +96,102 @@ function submitOrder() {
 
 // =======================================// Check Out Form Validation =================================================
 
+// ======================================= Membership Form Validation ===================================================
+function signupMember() {
+	var UserId = document.forms["formMembership"]["userid"].value;
+	var Password = document.forms["formMembership"]["password"].value;
+	var Fname = document.forms["formMembership"]["fname"].value;
+	var Lname = document.forms["formMembership"]["lname"].value;
+	var Email = document.forms["formMembership"]["email"].value;
+	var Code = document.forms["formMembership"]["code"].value;
+	
+	while(true){
+		var checkUserId = document.getElementById("errorUserId").innerHTML = '<img src="Images/icons/cross.png" width="12" height="12" alt="error">'+" Please enter your User ID";
+		var checkPassword = document.getElementById("errorPassword").innerHTML = '<img src="Images/icons/cross.png" width="12" height="12" alt="error">'+" Please enter your Password";
+		var checkTitle = document.getElementById("errorTitle").innerHTML = '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">';
+		var checkFname = document.getElementById("errorFname").innerHTML = '<img src="Images/icons/cross.png" width="12" height="12" alt="error">'+" Please enter your First Name";
+		var checkMname = document.getElementById("errorMname").innerHTML = '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">';
+		var checkLname = document.getElementById("errorLname").innerHTML = '<img src="Images/icons/cross.png" width="12" height="12" alt="error">'+" Please enter your Last Name";
+		var checkEmail = document.getElementById("errorEmail").innerHTML = '<img src="Images/icons/cross.png" width="12" height="12" alt="error">'+" Please enter a valid Email address";
+		var checkCountry = document.getElementById("errorCountry").innerHTML = '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">';
+		var checkCode = document.getElementById("errorCode").innerHTML = '<img src="Images/icons/cross.png" width="12" height="12" alt="error">'+" Please enter a valid Zip Code";
+		var checkAddress = document.getElementById("errorAddress").innerHTML = '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">';
+		var checkAbout = document.getElementById("errorAbout").innerHTML = '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">';
+		var checkNews = document.getElementById("errorNews").innerHTML = '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">';
+		var checkTerms;
+		
+		var checkMsg = checkUserId + checkPassword + checkTitle + checkFname + checkMname + checkLname +
+						checkEmail + checkCountry + checkCode + checkAddress + checkAbout + checkNews + checkTerms;
+		var popupMsg = null;
+		
+		var num = /^[0-9]+$/;
+		var atpos = Email.indexOf("@");
+    	var dotpos = Email.lastIndexOf(".");
+		
+		if(UserId !== null && UserId !== ""){
+			checkUserId = document.getElementById("errorUserId").innerHTML = '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">';
+			document.forms["formMembership"]["userid"].focus();
+		}
+		
+		if(Password !== null && Password !== ""){
+			checkPassword = document.getElementById("errorPassword").innerHTML = '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">';
+			document.forms["formMembership"]["password"].focus();
+		}
+		
+		if(Fname !== null && Fname !== ""){
+			checkFname = document.getElementById("errorFname").innerHTML = '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">';
+			document.forms["formMembership"]["fname"].focus();
+		}
+		
+		if(Lname !== null && Lname !== ""){
+			checkLname = document.getElementById("errorLname").innerHTML = '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">';
+			document.forms["formMembership"]["lname"].focus();
+		}
+		
+		if (atpos<1 || dotpos<atpos+2 || dotpos+2>=Email.length) {
+			checkEmail = document.getElementById("errorEmail").innerHTML = '<img src="Images/icons/cross.png" width="12" height="12" alt="error">'+" Please enter a valid Email address";
+			document.forms["formMembership"]["email"].focus();
+		}
+		else{
+			checkEmail = document.getElementById("errorEmail").innerHTML = '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">';
+		}
+		
+		if (!Code.match(num)){
+			checkCode = document.getElementById("errorCode").innerHTML = '<img src="Images/icons/cross.png" width="12" height="12" alt="error">'+" Please enter a valid Zip Code!";
+			document.forms["formMembership"]["code"].focus();
+			
+		}
+		else{
+			checkCode = document.getElementById("errorCode").innerHTML = '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">';
+		}
+		
+		if(document.getElementById('terms').checked == false){
+			checkTerms = document.getElementById("errorTerms").innerHTML = '<img src="Images/icons/cross.png" width="12" height="12" alt="error">'+" Please agree to the Terms and Conditions of Diazepam Cafe to Sign Up";
+			document.forms["formMembership"]["terms"].focus();
+		}
+		else{
+			checkTerms = document.getElementById("errorTerms").innerHTML = '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">';
+		}
+		
+		if(checkUserId == '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">' &&
+			checkPassword == '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">' &&
+			checkFname == '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">' &&
+			checkLname == '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">' &&
+			checkCode == '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">' && 
+			checkEmail == '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">' &&
+			checkCode == '<img src="Images/icons/tick.png" width="12" height="12" alt="tick">'){
+				popupMsg = alert("Thank you for Signing Up on Diazepam Cafe Website!");
+				if(popupMsg=true){
+					window.location="member.html";
+				}
+	
+			}
+			else{
+				popupMsg = alert("Please check your form again! All the required field must be filled properly!");
+			}
+		return false, checkMsg, popupMsg;
+	}
+	return true;
+}
+	
+// ======================================= Membership Form Validation ===================================================
